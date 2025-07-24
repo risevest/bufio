d2520fbc64985b1b1bebeee87278e0c2b620e03e
mcnoble1 <idowufestustemiloluwa@gmail.com>
2025-07-24 21:57:31 +0100

    chore: merged main :octocat:

--- src/storage/base.ts
+++ src/storage/base.ts
@@ -1,8 +1,4 @@
 export abstract class Storage<T> {
   abstract put(record: T): void;
-<<<<<<< HEAD
-  abstract get(): T[];
-=======
   abstract get(batchSize?: number): T[];
->>>>>>> main
 }
--- src/storage/memory.ts
+++ src/storage/memory.ts
@@ -7,14 +7,7 @@ export class MemoryStorage<T> implements Storage<T> {
     this.buffer.push(record);
   }
 
-<<<<<<< HEAD
-  get(): T[] {
-    const records = [...this.buffer];
-    this.buffer = [];
-    return records;
-=======
   get(batchSize = 1000): T[] {
     return this.buffer.splice(0, batchSize);
->>>>>>> main
   }
 }
